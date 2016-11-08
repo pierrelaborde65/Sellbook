32c0003b6f687408c5ee032381310d75537cbfb3
dv <dorian.vacher@gmail.com>
2016-11-08 10:35:56 +0100

    "Ajout tests User -> addUser"

---
+++ test/UserTest.java
@@ -0,0 +1,23 @@
+import controllers.ProductController;
+import controllers.UserController;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by dv on 08/11/2016.
+ */
+public class UserTest {
+
+    UserController uc = new UserController();
+
+    /*
+    @Test
+    public void addUser() {
+        int nbUser = uc.getUsers().size;
+        uc.addUser();
+        assertEquals(nbUser+1,uc.getUsers().size);
+    }
+    */
+}
