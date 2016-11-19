66d042b51c278a50906367af5747350978a4b07b
johannesMeier <maildejohannes.34@gmail.com>
2016-11-19 21:25:57 +0100

    Securuté pages admin + petit modif + design page index

--- public/javascripts/allSellers.js
+++ public/javascripts/allSellers.js
@@ -4,6 +4,25 @@ moduleSellbook.controller('allSellers', function($scope, $http, $window, $cookie
     var idUser = $cookies.get('id');
     var tokenUser = $cookies.get('token');
 
+           //--------------------- Check Admin ----------------------------------------------------
+    if(!angular.isUndefined(idUser) && !angular.isUndefined(tokenUser)){
+            var rqt = {
+                method : 'GET',
+                url : '/isConnected/' + idUser + '/' + tokenUser,
+                data : $.param({id: idUser, token: tokenUser}),
+                headers : { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
+            };
+            $http(rqt).success(function(data){
+                // If the USER is a SC ok else redirect /
+                if(data["statusUser"] != 2) {
+                    $window.location.href = '/';
+                }
+            });
+    }
+    else {
+        $window.location.href = '/';
+    }
+
 
     // Hide the error message at the beginning
     $scope.hideError = true;
